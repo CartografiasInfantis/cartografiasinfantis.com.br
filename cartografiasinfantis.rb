e80f53cc21f335ca8dae075b514df4eb6f6a77b3
Rafael Bandeira <rafaelbandeira3@gmail.com>
2011-07-14 19:45:26 -0300

    adding templates

--- cartografiasinfantis.rb
+++ cartografiasinfantis.rb
@@ -1,5 +1,10 @@
 module CartografiasInfantis
   class App < Sinatra::Base
     set :public, File.dirname(__FILE__) + '/public'
+    set :views , File.dirname(__FILE__) + '/templates'
+
+    get '/' do
+      erb :index
+    end
   end
 end
