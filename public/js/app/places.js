a7a29218620d13b0d9274014b803503a9d1a5267
Rafael Bandeira <rafaelbandeira3@gmail.com>
2011-07-20 03:39:26 -0300

    places controller and gmaps datasource

--- public/js/app/places.js
+++ public/js/app/places.js
@@ -1,2 +1,47 @@
-var CartografiasInfantis = window.CartografiasInfantis || {};
-CartografiasInfantis.Places = {};
+var CartografiasInfantis = window.CartografiasInfantis || {}
+  , Map = CartografiasInfantis.Map
+  , Places = CartografiasInfantis.Places = {};
+
+
+Places.Place = function(props) {
+  props = props || {};
+  this.coordinates = props.coordinates || {};
+  this.name = props.name || '';
+}
+
+Places.PlacesController = function() {
+  var methods = Places.PlacesController.InstanceMethods;
+  
+  for (var i in methods) {
+    if (methods.hasOwnProperty(i)) {
+      this[i] = methods[i];
+    }
+  }
+
+  this.collection = [];
+}
+
+Places.PlacesController.prototype = new CartografiasInfantis.Broadcaster();
+
+Places.PlacesController.InstanceMethods = {
+  addPlace: function(place) {
+    this.collection.push(place);
+    this.broadcast('places:added', [place, this]);
+  }
+}
+
+Places.GMapsDataSource = function() {
+}
+
+Places.GMapsDataSource.prototype = {
+  getPlaceData: function(address, callback) {
+    Map.Api.geocodeAddress(address, function(placeData) {
+      var place = new Places.Place({
+        coordinates: Map.Api.getCoordinatesOf(placeData),
+        name: address
+      });
+
+      callback.call(window, place);
+    });
+  }
+}
--- spec/javascripts/places_spec.js
+++ spec/javascripts/places_spec.js
@@ -1,2 +1,54 @@
 describe('Places', function() {
+  var Places = CartografiasInfantis.Places;
+    
+  describe('PlacesController', function() {
+    var controller;
+
+    beforeEach(function() {
+      controller = new Places.PlacesController();
+    });
+    
+    it('should broadcast places added', function() {
+      var mockObserver = jasmine.createSpy('Event observer')
+        , addedPlace = new Places.Place();
+
+      controller.registerObserver(mockObserver, 'places:added');
+      controller.addPlace(addedPlace);
+
+      expect(mockObserver).toHaveBeenCalledWith(addedPlace, controller);
+    });
+  });
+
+  describe('GMapsDataSource', function() {
+    var dataSource;
+
+    beforeEach(function() {
+      dataSource = new Places.GMapsDataSource();
+    });
+
+    it('should create places async with gmaps data of a given address', function() {
+      var mockCallback = jasmine.createSpy('DataSource callback')
+        , stubAddress = 'foo address'
+        , stubCoordinates = {foo: 'coordinates'}
+        , mockGeocoderResponse = {
+            geometry: {location: stubCoordinates}
+          };
+
+      spyOn(Map.Api, 'geocodeAddress').andCallFake(function(addr, callback) {
+        setTimeout(function() {
+          callback.call(window, mockGeocoderResponse);
+        }, 0);
+      });
+
+      dataSource.getPlaceData(stubAddress, mockCallback);
+
+      waitsFor(function() { return mockCallback.wasCalled; });
+      runs(function() {
+        var callbackArgument = mockCallback.mostRecentCall.args[0];
+        expect(callbackArgument instanceof Places.Place).toBeTruthy();
+        expect(callbackArgument.coordinates).toEqual(stubCoordinates);
+      });
+    });
+  });
 });
+
