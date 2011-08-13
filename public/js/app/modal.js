5b1530371c7a3aefc4af607e3f66457ff654264e
Natalia Arsand <nataliarsand@gmail.com>
2011-08-13 15:46:05 -0300

    title helper for modal window

--- public/js/app/modal.js
+++ public/js/app/modal.js
@@ -26,6 +26,20 @@ UI.Modal.prototype.addContent = function(content) {
   this.getElement().append(content);
 }
 
+UI.Modal.prototype.setTitle = function(text) {
+  this.getTitle().text(text);
+}
+
+UI.Modal.prototype.getTitle = function() {
+  var element = this.getElement();
+  var title = element.find('h1');
+  if (title.length == 0)  {
+    title = $('<h1>');
+    element.prepend(title);
+  }
+  return title;
+}
+
 UI.Modal.prototype.close = function() {
   this.getElement().hide();
 }
